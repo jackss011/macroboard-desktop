4ef83929cb5636587b0a86b8f489f3f62aada863
jackss011 <jackgaming81@gmail.com>
2017-05-04 00:43:59 +0200

    Moved TcpConnection as an inner class of TcpService

--- src/java/macroboard/network/BeaconService.java
+++ src/java/macroboard/network/BeaconService.java
@@ -1,5 +1,6 @@
 package macroboard.network;
 
+import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import macroboard.settings.StaticSettings;
 import macroboard.utility.Log;
@@ -13,45 +14,37 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+
 /**
- *
+ *  Service responsible to broadcast UDP packets in order to be found by other devices.
  */
-public class Beacon
+class BeaconService extends Service
 {
-    private BeaconTask beaconTask;
-
-    private ExecutorService executorService;
-
-    private Future taskFuture;
-
-
-// |==========================
-// |==>  CLASSES
-// |==========================
-
     private class BeaconTask extends Task
     {
+        private MulticastSocket receiver;
+
         @Override
         protected Void call() throws Exception
         {
             System.out.println("Running");
 
-            try(MulticastSocket receiver = new MulticastSocket(StaticSettings.NET_PORT);
-                DatagramSocket responder = new DatagramSocket())
+            try (DatagramSocket responder = new DatagramSocket())
             {
+                receiver = new MulticastSocket(StaticSettings.NET_PORT);
                 receiver.joinGroup(InetAddress.getByName(StaticSettings.MULTICAST_ADDRESS));
 
-                while(true)
+                while (true)
                 {
                     byte[] buff = new byte[256];
                     DatagramPacket packet = new DatagramPacket(buff, buff.length);
                     receiver.receive(packet);
 
-                    if (Thread.interrupted()) break;
+                    if (isCancelled()) break;
 
                     String requestData = new String(packet.getData(), packet.getOffset(), packet.getLength(), StandardCharsets.UTF_8);
 
-                    if(true) //TODO: check data packet data here
+                    if (true) //TODO: check data packet data here
                     {
                         InetAddress senderAddress = packet.getAddress();
                         System.out.println("Received from: " + senderAddress.getHostAddress());
@@ -65,7 +58,6 @@ public class Beacon
                                 StaticSettings.NET_PORT);
 
                         responder.send(responsePacket);
-
                     }
                 }
             }
@@ -73,56 +65,24 @@ public class Beacon
             {
                 e.printStackTrace();
             }
+            finally
+            {
+                if(receiver != null) receiver.close();
+            }
 
             return null;
         }
-    }
 
-
-
-// |==========================
-// |==>  CONSTRUCTORS
-// |==========================
-
-    public Beacon()
-    {
-        executorService = Executors.newSingleThreadExecutor(r ->
+        @Override
+        protected void cancelled()
         {
-            Thread t = new Thread(r);
-            t.setDaemon(true);
-            return t;
-        });
-    }
-
-
-
-// |==========================
-// |==>  METHODS
-// |==========================
-
-    public boolean isRunning()
-    {
-        return taskFuture != null && !taskFuture.isDone() && !taskFuture.isCancelled();
-    }
-
-    public void start()
-    {
-        if(!isRunning())
-        {
-            Log.d("Running beacon");
-            beaconTask = new BeaconTask();
-            taskFuture = executorService.submit(new BeaconTask());
+            if(receiver!= null) receiver.close();
         }
     }
 
-    public void stop()
+    @Override
+    protected Task createTask()
     {
-        if(isRunning())
-        {
-            Log.d("Stopping beacon");
-            taskFuture.cancel(true);
-            taskFuture = null;
-        }
+        return new BeaconTask();
     }
-
 }
